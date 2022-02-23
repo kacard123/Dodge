6f4327c65287101efc959283fd0d1eddae2f2f20
kacard123 <shalvah623@naver.com>
2022-02-23 17:26:24 +0900

    20220223_Last

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //이동에 사용할 리지드바디 컴포넌트
+    public Rigidbody plyerRigidbody;
+    // 이동속력
+    public float speed = 8f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
