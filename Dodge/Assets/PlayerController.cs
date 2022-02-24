25e1ab372cddd0cd9bd65cffac2bb8cff63b3a20
MinSeo <gold7098@naver.com>
2022-02-24 17:32:19 +0900

    20220224_Last

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //이동에 사용할 리지드바디 컴포넌트
-    public Rigidbody plyerRigidbody;
+    public Rigidbody playerRigidbody;
     // 이동속력
-    public float speed = 8f;
-    
+    public float speed = 3f;
+
+    public GameObject my; 
+
+    private void start()
+    {
+        //게임 오브젝트에서 Rigidbody컴포넌트를 찾아 playerrigidbody에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         
@@ -17,6 +24,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        float xInput = Input.GetAxis("Horizontal");
+        // 키보드 : 'A' < - : 음의 방향 : 최대 -1.0f
+        //키보드 : 'D' -> : 양의 방향 :  +1.0f
+        float zInput = Input.GetAxis("Vertical");
+        //키보드 : 'W',^ : 양의방향 : +1.0f
+        //키보드 : 'S', ▼ : 음의방향 : -1.0f
+    
+    }
+
+ 
+        void DirectInput()
+        {
+            if (Input.GetKey(KeyCode.UpArrow) == true)
+            {
+                playerRigidbody.AddForce(0f, 0f, speed);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) == true)
+            {
+                playerRigidbody.AddForce(-speed, 0f, 0f);
+
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) == true)
+            {
+                playerRigidbody.AddForce(speed, 0f, 0f);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow) == true)
+            {
+                playerRigidbody.AddForce(0f, 0f, -speed);
+            }
+        }
+   
+   
+    void Die()
+    {
+        my.SetActive(false);
+        
     }
 }
